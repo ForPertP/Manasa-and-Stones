cee9e8d61067d161785c796cd6a49e84943b5534
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-09-24 18:11:30 +0900

    Update Manasa-and-Stones.java

--- Manasa-and-Stones.java
+++ Manasa-and-Stones.java
@@ -10,3 +10,35 @@ import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int T = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, T).forEach(TItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int a = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int b = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> result = Result.stones(n, a, b);
+
+                bufferedWriter.write(
+                    result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                    + "\n"
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
